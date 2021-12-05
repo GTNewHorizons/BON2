e524867d34187f2c5f277fc76f96f01be6cf9d84
glowredman <fredcraft00@gmail.com>
2021-12-05 17:51:53 +0100

    Update mapping URLs

--- src/main/java/com/github/parker8283/bon2/data/GuiDownloadNew.java
+++ src/main/java/com/github/parker8283/bon2/data/GuiDownloadNew.java
@@ -29,8 +29,8 @@ import com.google.common.collect.Lists;
 
 public class GuiDownloadNew extends JFrame {
     
-    private static final String MAPPINGS_URL_SNAPSHOT = "http://export.mcpbot.bspk.rs/mcp_snapshot/%1$s-%2$s/mcp_snapshot-%1$s-%2$s.zip";
-    private static final String MAPPINGS_URL_STABLE = "http://export.mcpbot.bspk.rs/mcp_stable/%1$s-%2$s/mcp_stable-%1$s-%2$s.zip";
+    private static final String MAPPINGS_URL_SNAPSHOT = "https://maven.minecraftforge.net/de/oceanlabs/mcp/mcp_snapshot/%1$s-%2$s/mcp_snapshot-%1$s-%2$s.zip";
+    private static final String MAPPINGS_URL_STABLE = "https://maven.minecraftforge.net/de/oceanlabs/mcp/mcp_stable/%1$s-%2$s/mcp_stable-%1$s-%2$s.zip";
     
     private static class MappingListEntry {
         public final boolean stable;
